58218642ffe6e3c50dc0627d630049ad27c94f3e
Tiago Souza <tiagoadesouza21@gmail.com>
2025-08-13 09:18:29 -0300

    first commit

---
+++ src/main/java/com/example/demo/ItauUnibancoDesafioDeProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauUnibancoDesafioDeProgramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauUnibancoDesafioDeProgramacaoApplication.class, args);
+	}
+
+}
