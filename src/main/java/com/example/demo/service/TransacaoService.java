a9579d6bf35bb9748e2fd7ae9752e68928499fb8
Tiago Azevedo <tiagoadesouza21@gmail.com>
2025-08-13 21:58:00 -0300

    adicionando alguns Logs

--- src/main/java/com/example/demo/service/TransacaoService.java
+++ src/main/java/com/example/demo/service/TransacaoService.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.EstatisticaDto;
 import com.example.demo.dto.TransacaoDto;
 import com.example.demo.model.Transacao;
 import com.example.demo.repository.TransacaoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class TransacaoService {
 
     private final TransacaoRepository transacaoRepository;
@@ -22,25 +24,30 @@ public class TransacaoService {
 
     public Transacao salvarTransacao(TransacaoDto dto){
         if (dto.dataHora().isAfter(OffsetDateTime.now())){
+            log.error("Data/Hora da transação maior que a data atual");
             throw new IllegalArgumentException();
         }
 
         Transacao transacao = new Transacao(dto);
         transacaoRepository.salvar(transacao);
+        log.info("Transação salva com sucesso");
         return transacao;
     }
 
     public void deletarTransacoes(){
         transacaoRepository.deletarTudo();
+        log.info("Transações deletadas com sucesso");
     }
 
     public EstatisticaDto retornarEstatisticas(){
         List<Double> valores = transacaoRepository.retornarValores();
         if (valores.isEmpty()){
+            log.info("Estatisticas calculadas com sucesso");
             return new EstatisticaDto(0, 0.0, 0.0, 0.0, 0.0);
         }
         DoubleSummaryStatistics stats = valores.stream().collect(Collectors.summarizingDouble(Double::doubleValue));
 
+        log.info("Estatisticas calculadas com sucesso");
         return new EstatisticaDto(stats.getCount(), stats.getSum(), stats.getAverage(), stats.getMin(), stats.getMax());
     }
 
